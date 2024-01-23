faf33115c69834e35c0a5408176f902c036d2ba5
dduniverse <suhyunbory@gmail.com>
2024-01-23 15:58:31 +0900

    로그아웃 기능 구현

--- plands/settings.py
+++ plands/settings.py
@@ -122,5 +122,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, "plands", "static")]
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
-# 로그인 성공 시 자동으로 이동할 URL
-LOGIN_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/'        # 로그인 성공 시 자동으로 이동할 URL
+LOGOUT_REDIRECT_URL = '/'       # 로그아웃 성공 시 자동으로 이동할 URL
